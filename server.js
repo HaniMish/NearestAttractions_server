6130f0093830fba237d07aca6674ed3c9c8f6896
Hani Mishkovsky <chanistern123@gmail.com>
2022-07-04 11:09:12 +0300

    client port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000
 
-const whitelist = ["http://localhost:3000"]
+const whitelist = ["http://localhost:8081"]
 var corsOptions = {
   origin: function (origin, callback) {
     if (!origin || whitelist.indexOf(origin) !== -1) {
